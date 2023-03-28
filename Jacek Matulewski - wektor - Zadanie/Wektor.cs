ab45063c0d66d546ec96b0b615fb75506b7851b0
Fi Szer <filipsz60@wp.pl>
2023-03-28 23:49:15 +0200

    Dodaj pliki projektów.

---
+++ Jacek Matulewski - wektor - Zadanie/Wektor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace WektorBiblioteka
+{
+    public struct Wektor
+    {
+        private double wx, wy, wz;
+        public Wektor(double wx, double wy, double wz)
+        {
+            this.wx = wx;
+            this.wy = wy;
+            this.wz = wz;
+        }
+        public static readonly Wektor Zerowy = new Wektor(0, 0, 0);
+        public static readonly Wektor Jednostkowy = new Wektor(1, 1, 1);
+
+        public static string Info()
+        {
+            return "Struktura Wektor";
+        }
+
+        public override string ToString()
+        {
+            return ("("+wx + ", "+wy +", "+wz+")").ToString();
+        }
+        public double DługośćWektora()
+        {
+            double WX = wx;
+            double WY = wy;
+            double WZ = wz;
+            return Math.Sqrt(WX *WX + WY * WY + WZ * WZ);
+        }
+
+        #region Własności
+        public double Wx { get => wx; set => wx = value; }
+        public double Wy
+        {
+            get { return wy; }
+            set { wy = value; }
+        }
+        public double Wz
+        {
+            get { return wz; }
+            set { wz = value; }
+        }
+
+        
+        #endregion
+    }
+
+}
---
+++ WektorPrezentacja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WektorBiblioteka;
+
+namespace WektorPrezentacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Wektor.Info());
+            Wektor W0 = Wektor.Zerowy;
+            Wektor W1 =  Wektor.Jednostkowy;
+            Wektor W2 = new Wektor(3, 6, 6);
+            Wektor W3 = new Wektor() { Wx = 3, Wy = 4, Wz = 5 };
+
+            Console.WriteLine(W0);
+            Console.WriteLine(W1);
+            Console.WriteLine(W2);
+            Console.WriteLine(W3);
+
+
+            Console.WriteLine(W0.DługośćWektora());
+            Console.WriteLine(W1.DługośćWektora());
+            Console.WriteLine(W2.DługośćWektora());
+            Console.WriteLine(W3.DługośćWektora());
+        }
+    }
+}
