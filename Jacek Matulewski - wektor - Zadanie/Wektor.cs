1a6671639e9c39cf48817e04a4d40526676d4d31
Fi Szer <filipsz60@wp.pl>
2023-03-29 13:16:21 +0200

    Dodano operatory i parę funkcji

--- Jacek Matulewski - wektor - Zadanie/Wektor.cs
+++ Jacek Matulewski - wektor - Zadanie/Wektor.cs
@@ -2,50 +2,130 @@
 
 namespace WektorBiblioteka
 {
-    public struct Wektor
+    public struct Wektor : IComparable<Wektor>
     {
-        private double wx, wy, wz;
         public Wektor(double wx, double wy, double wz)
         {
-            this.wx = wx;
-            this.wy = wy;
-            this.wz = wz;
+            this.Wx = wx;
+            this.Wy = wy;
+            this.Wz = wz;
         }
+        #region Własności
+        public double Wx { get; set; }
+        public double Wy { get; set; }
+        public double Wz { get; set; }
+        #endregion
+
         public static readonly Wektor Zerowy = new Wektor(0, 0, 0);
         public static readonly Wektor Jednostkowy = new Wektor(1, 1, 1);
 
+        #region Funkcje
         public static string Info()
         {
             return "Struktura Wektor";
         }
-
+        public static Wektor RandomWektor()
+        {
+            Random r = new Random();
+            return new Wektor(r.Next(10), r.Next(10), r.Next(10));
+        }
+        public int CompareTo(Wektor A)
+        {
+            return Math.Sign(this.DługośćWektora() - A.DługośćWektora());
+        }
         public override string ToString()
         {
-            return ("("+wx + ", "+wy +", "+wz+")").ToString();
+            return ("(" + Wx + ", " + Wy + ", " + Wz + ")").ToString();
         }
-        public double DługośćWektora()
+        public readonly double DługośćWektora()
         {
-            double WX = wx;
-            double WY = wy;
-            double WZ = wz;
-            return Math.Sqrt(WX *WX + WY * WY + WZ * WZ);
+            return Math.Sqrt(Wx * Wx + Wy * Wy + Wz * Wz);
+        }
+        public static double Dot(Wektor A, Wektor B) //iloczyn skalarny
+        {
+            return (A.Wx * B.Wx + A.Wy * B.Wy + A.Wz * B.Wz);
+        }
+        public static double Dot(Wektor A, Wektor B, double kąt) //iloczyn skalarny
+        {
+            return (A.DługośćWektora() * B.DługośćWektora() * Math.Cos(kąt * Math.PI / 180));
+        }
+        public static Wektor Cross(Wektor A, Wektor B) //iloczyn wektorowy
+        {
+            return new Wektor(B.Wy * A.Wz - B.Wz * A.Wy, B.Wz * A.Wx - B.Wx * A.Wz, B.Wx * A.Wy - B.Wy * A.Wx);
+        }
+        public static double DługośćIloczynuWektorów(Wektor A, Wektor B, double kąt) //iloczyn wektorowy
+        {
+            return (A.DługośćWektora() * B.DługośćWektora() * Math.Sin(kąt * Math.PI / 180));
+        }
+        public static bool JestProstopadły(Wektor A, Wektor B)
+        {
+            if (A == Zerowy || B == Zerowy) throw new Exception("Jeden z wektorów jest zerowy!");
+            return ((Math.Acos((Dot(A, B)) / (A.DługośćWektora() * B.DługośćWektora()))) == Math.PI / 2) ? true : false;
+        }
+        public override bool Equals(object Obj)
+        {
+            if (!(Obj is Wektor)) return false;
+            Wektor A = (Wektor)Obj;
+            return (this == A);
+        }
+        public override int GetHashCode()
+        {
+            return (int)Wx ^ (int)Wy ^ (int)Wz;
         }
 
-        #region Własności
-        public double Wx { get => wx; set => wx = value; }
-        public double Wy
-        {
-            get { return wy; }
-            set { wy = value; }
-        }
-        public double Wz
-        {
-            get { return wz; }
-            set { wz = value; }
-        }
 
-        
+
         #endregion
+        #region Operatory
+        public static Wektor operator +(Wektor A)
+        {
+            return A;
+        }
+        public static Wektor operator +(Wektor A, Wektor B)
+        {
+            return new Wektor(A.Wx + B.Wx, A.Wy + B.Wy, A.Wz + B.Wz);
+        }
+        public static Wektor operator -(Wektor A)
+        {
+            return new Wektor(-A.Wx, -A.Wy, -A.Wz);
+        }
+        public static Wektor operator -(Wektor A, Wektor B)
+        {
+            return new Wektor(A.Wx - B.Wx, A.Wy - B.Wy, A.Wz - B.Wz);
+        }
+        public static double operator *(Wektor A, double B)
+        {
+            return (A.Wx * B + A.Wy * B + A.Wz * B);
+        }
+        public static double operator *(double B, Wektor A)
+        {
+            return (A.Wx * B + A.Wy * B + A.Wz * B);
+        }
+        public static double operator /(Wektor A, double B)
+        {
+            if (B == 0) throw new Exception("Nie można podzielić wektora przez zero!");
+            return (A.Wx / B + A.Wy / B + A.Wz / B);
+        }
+        public static double operator *(Wektor A, Wektor B) //iloczyn skalarny
+        {
+            return (A.Wx * B.Wx + A.Wy * B.Wy + A.Wz * B.Wz);
+        }
+        public static Wektor operator ^(Wektor A, Wektor B) //iloczyn wektorowy
+        {
+            return new Wektor(B.Wy * A.Wz - B.Wz * A.Wy, B.Wz * A.Wx - B.Wx * A.Wz, B.Wx * A.Wy - B.Wy * A.Wx);
+        }
+        public static bool operator !=(Wektor A, Wektor B)
+        {
+            return (A.Wx == B.Wx && A.Wy == B.Wy && A.Wz == B.Wz) ? false : true;
+        }
+        public static bool operator ==(Wektor A, Wektor B)
+        {
+            return (A.Wx == B.Wx && A.Wy == B.Wy && A.Wz == B.Wz) ? true : false;
+        }
+        #endregion
+
     }
+
+
 
 }
--- WektorPrezentacja/Program.cs
+++ WektorPrezentacja/Program.cs
@@ -9,20 +9,39 @@ namespace WektorPrezentacja
         {
             Console.WriteLine(Wektor.Info());
             Wektor W0 = Wektor.Zerowy;
-            Wektor W1 =  Wektor.Jednostkowy;
-            Wektor W2 = new Wektor(3, 6, 6);
-            Wektor W3 = new Wektor() { Wx = 3, Wy = 4, Wz = 5 };
+            Wektor W1 = Wektor.Jednostkowy;
+            Wektor W2 = new Wektor(2, 1, 3);
+            Wektor W3 = new Wektor() { Wx = 3, Wy = -3, Wz = -1 };
 
             Console.WriteLine(W0);
             Console.WriteLine(W1);
             Console.WriteLine(W2);
             Console.WriteLine(W3);
 
+            Console.WriteLine();
+            Console.WriteLine(Wektor.JestProstopadły(W2, W3));
+            Console.WriteLine(W2 * W3);
 
+            Console.WriteLine();
             Console.WriteLine(W0.DługośćWektora());
             Console.WriteLine(W1.DługośćWektora());
             Console.WriteLine(W2.DługośćWektora());
             Console.WriteLine(W3.DługośćWektora());
+
+            Console.WriteLine();
+            Wektor[] tab = new Wektor[10];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = Wektor.RandomWektor();
+            }
+            foreach (Wektor element in tab) Console.WriteLine(element + "  " + element.DługośćWektora());
+
+            Console.WriteLine();
+            Array.Sort(tab);
+            foreach (Wektor element in tab) Console.WriteLine(element + "  " + element.DługośćWektora());
+
+
+
         }
     }
 }
